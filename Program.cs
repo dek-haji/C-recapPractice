c220e81aa430e03bc0487aa02fcbbe6809179ce7
dek-haji <deeeqhaji@gmail.com>
2019-10-03 13:15:22 -0500

    used condtions

--- Program.cs
+++ Program.cs
@@ -8,30 +8,30 @@ namespace linqExercises
          static void Main(string[] args)
         {
             // DateTime is the type of the purchaseData variable.
-            DateTime purchaseDate=DateTime.Now;
+            // DateTime purchaseDate=DateTime.Now;
 
-            /*
-                string is the type of the lastName variable. It
-                tells the compiler that the lastName variable can
-                ONLY hold a string value.
-            */
-            string lastName="Dek";
+            // /*
+            //     string is the type of the lastName variable. It
+            //     tells the compiler that the lastName variable can
+            //     ONLY hold a string value.
+            // */
+            // string lastName="Dek";
 
-            /*
-                C# now supports implicitly typing of a variable. The
-                type of the variable will be determined, by the
-                compiler, at compile time.
-            */
-            var firstName="Haji";
+            // /*
+            //     C# now supports implicitly typing of a variable. The
+            //     type of the variable will be determined, by the
+            //     compiler, at compile time.
+            // */
+            // var firstName="Haji";
 
-            /*
-                String interpolation in C# is similar to string interpolation in JavaScript,
-                but there is a different syntax.
+            // /*
+            //     String interpolation in C# is similar to string interpolation in JavaScript,
+            //     but there is a different syntax.
 
-                An equivalent statement in JavaScript would be:
-                console.log(`${firstName} ${lastName} purchased on ${purchaseDate}`);
-            */
-            Console.WriteLine($"{firstName} {lastName} purchased on {purchaseDate}");
+            //     An equivalent statement in JavaScript would be:
+            //     console.log(`${firstName} ${lastName} purchased on ${purchaseDate}`);
+            // */
+            // Console.WriteLine($"{firstName} {lastName} purchased on {purchaseDate}");
 
 
             List<string> Products = new List<string>(){
@@ -43,23 +43,34 @@ namespace linqExercises
                 "Imac",
                 "Iwatch"
             };
-            //for each loop is used to iterate over a collection
-            foreach (string Product in Products){
-                Console.WriteLine(Product);
-            }
+            // //for each loop is used to iterate over a collection
+            // foreach (string Product in Products){
+            //     Console.WriteLine(Product);
+            // }
 
-              List<string> RandomStuff = new List<string>(){
-                "Sofa",
-                "Cake",
-                "Mirror",
-                "random",
-                "Iphone",
-                "Imac",
-                "Iwatch"
-            };
+            //   List<string> RandomStuff = new List<string>(){
+            //     "Sofa",
+            //     "Cake",
+            //     "Mirror",
+            //     "random",
+            //     "Iphone",
+            //     "Imac",
+            //     "Iwatch"
+            // };
             //for loop can be used to and to get the equavalent the foreach above
-            for (int i=0; i<RandomStuff.Count; i++){
-                Console.WriteLine(RandomStuff[i]);
+            // for (int i=0; i<RandomStuff.Count; i++){
+            //     Console.WriteLine(RandomStuff[i]);
+            // }
+
+            //now lets use conditions in c#
+            foreach( string product in Products){
+                if(product.Length < 5){
+                    Console.WriteLine($"{product} has a short names");
+                }else if(product.Length < 10){
+                    Console.WriteLine($"{product} has a medium-sized names");
+                }else {
+                    Console.WriteLine($"{product} has a long names");
+                }
             }
 
         }
